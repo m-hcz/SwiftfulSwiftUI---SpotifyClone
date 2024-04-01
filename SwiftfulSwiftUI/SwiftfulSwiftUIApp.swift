777417b6a00f5cecceb23cca2656e4c6e13a1585
Michal Heczko <heczkom6@gmail.com>
2024-04-01 10:44:08 +0200

    Initial Commit

---
+++ SwiftfulSwiftUI/SwiftfulSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIApp.swift
+//  SwiftfulSwiftUI
+//
+//  Created by mhcz on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
